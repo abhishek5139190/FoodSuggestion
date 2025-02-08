1d660e7bebf309f705d3a5be7b63904af65ca9d9
Abhishek Mishra <abhishek5139190@users.noreply.github.com>
2025-02-08 15:53:12 +0530

    Update main.py
    Update the code from tweet to food suggestion

--- main.py
+++ main.py
@@ -7,31 +7,33 @@ import os
 
 os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 
-# Create prompt template for generating tweets
-
-tweet_template = "Give me {number} tweets on {topic}"
-
-tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic'])
-
 # Initialize Google's Gemini model
-gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
+gemini_model = ChatGoogleGenerativeAI(model = "gemini-2.0-flash-thinking-exp-01-21")
+
+# Create prompt template for suggesting food
+
+food_template = " For city {city}, suggest me top {number} food names along with name and address of  best place or resturant where i can get the food "
+
+food_prompt = PromptTemplate(template = food_template, input_variables = ['city', 'number'])
+
+
 
 
 # Create LLM chain using the prompt template and model
-tweet_chain = tweet_prompt | gemini_model
+food_chain = food_prompt | gemini_model
 
 
 import streamlit as st
 
-st.header("Tweet Generator - SATVIK")
+st.header("Food Suggestion App - Abhishek")
 
-st.subheader("Generate tweets using Generative AI")
+st.subheader("Suggest food which is famous in city along with places to eat")
 
-topic = st.text_input("Topic")
+city = st.text_input("Enter city Name")
 
-number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1, step = 1)
+number = st.number_input("Number of food suggestion", min_value = 1, max_value = 10, value = 1, step = 1)
 
-if st.button("Generate"):
-    tweets = tweet_chain.invoke({"number" : number, "topic" : topic})
-    st.write(tweets.content)
+if st.button("Find me Food"):
+    food = food_chain.invoke({"city" : city, "number" : number})
+    st.write(food.content)
     
